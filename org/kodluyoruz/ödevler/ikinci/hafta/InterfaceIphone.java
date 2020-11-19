bb61492abb07bbed11685829d4167ad2ac32be69
kubracebbar <50906289+kubracebbar@users.noreply.github.com>
2020-11-19 22:39:37 +0300

    Add files via upload

---
+++ org/kodluyoruz/ödevler/ikinci/hafta/InterfaceIphone.java
@@ -0,0 +1,7 @@
+package org.kodluyoruz.ödevler.ikinci.hafta;
+
+public interface InterfaceIphone {
+    void sms();
+
+    void videoCall();
+}
---
+++ org/kodluyoruz/ödevler/ikinci/hafta/InterfaceSamsung.java
@@ -0,0 +1,8 @@
+package org.kodluyoruz.ödevler.ikinci.hafta;
+
+public interface InterfaceSamsung {
+
+    void fileSharing();
+
+    void mp3();
+}
